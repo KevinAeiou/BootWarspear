24968261e5dc89bf70af6981db42894af7f88a02
Kevin <kevin.souzaeiou@outlook.com>
2023-07-01 11:25:17 -0400

    Implementação: retornaVidaMana()

--- metodos/manipula_tela.py
+++ metodos/manipula_tela.py
@@ -4,6 +4,16 @@ import manipula_cliente
 import manipula_funcoes
 import manipula_imagem
 import pyautogui as tecla
+import numpy as np
+
+# vida=58,mana=78
+# esquerda=38,direita=1194
+esquerda=39
+direita=509
+vida=65
+mana=85
+
+corVidaMana=(0,0,148)#vermelho
 
 def busca_tecla_especifica():
     a = win32api.GetKeyState(0x01)
@@ -20,8 +30,21 @@ def desenhaMiniMapa():
     metadeAltura=alturaTela//2
     pixelInicial=[metadeAltura-150,metadeLargura-50]
     # while pixelInicial!=(0,0,0).all():
+    # manipula_imagem.mostra_imagem(0,frameTela,None)
 
+def retornaVidaMana(vidaMana,posicao):
+    if vidaMana==85:
+        corVidaMana=(148,99,0)#azul
+    telaInteira=manipula_funcoes.retorna_atualizacao_tela()
+    frameTela=telaInteira[vidaMana:vidaMana+1,posicao:posicao+133]
+    contadorPixelVermelho=np.sum(frameTela==corVidaMana)
+    return retornaPorcentagemVida(contadorPixelVermelho)
 
-    manipula_imagem.mostra_imagem(0,frameTela,None)
+def retornaPorcentagemVida(pixelVermelho):
+    porcentagem=0
+    if pixelVermelho!=0:
+        porcentagem=(pixelVermelho*100)//399
+    return porcentagem
 
-desenhaMiniMapa()
+# print(f'{retornaVidaMana(vida,esquerda)}%')
+# desenhaMiniMapa()
